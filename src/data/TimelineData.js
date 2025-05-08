dbe1ef3036c4525be222d4a8674d0cea5db54f8a
Your Name <you@example.com>
2025-05-08 19:04:14 +0200

    added search params for opening tiny house modal gallery

---
+++ src/data/TimelineData.js
@@ -0,0 +1,256 @@
+export const timelineData = [
+  {
+    year: "2006-2012",
+    title_en: "High School Diploma",
+    title_de: "Realschulabschluss",
+    text_en:
+      "High School Diploma Realschule in Eisenberg, Rhineland-Palatinate, Germany. " +
+      "Elective: Computer Science and Technology.",
+    text_de:
+      "Realschulabschluss an der Realschule in Eisenberg, Rheinland-Pfalz, Deutschland. " +
+      "Wahlfach: Informatik und Technik.",
+  },
+  {
+    year: "2012-2015",
+    title_en: "Apprenticeship",
+    title_de: "Ausbildung",
+    text_en:
+      "Apprenticeship at the company <a href='http://www.itlutz.de/homepage/index.html' class='timeline-link'>Informationstechnik Lutz</a><span class='link-arrow'>&#129133;</span> in Gehrweiler, Rhineland-Palatinate, Germany. " +
+      "Attended Vocational School <a href='https://bbs1-kl.de/' class='timeline-link'>BBS1-KL</a><span class='link-arrow'>&#129133; </span>in Kaiserslautern, Rhineland-Palatinate, Germany. " +
+      "More information about the profession <a class='timeline-link' href='https://www.dqr.de/dqr/shareddocs/qualifikationen-neu/en/Information-electronics-technician-m-f-equipment-and-systems-technology.html'>here</a><span class='link-arrow'>&#129133;</span>",
+    text_de:
+      "Ausbildung bei der Firma <a href='http://www.itlutz.de/homepage/index.html' class='timeline-link'>Informationstechnik Lutz</a><span class='link-arrow'>&#129133;</span> in Gehrweiler, Rheinland-Pfalz, Deutschland. " +
+      "Besuch der Berufsschule <a href='https://bbs1-kl.de/' class='timeline-link'>BBS1-KL</a><span class='link-arrow'>&#129133; </span> in Kaiserslautern, Rheinland-Pfalz, Deutschland." +
+      "Mehr informationen zum Berufsbild <a href='https://www.dqr.de/dqr/shareddocs/qualifikationen-neu/de/Informationselektroniker-Schwerpunkt-Geraete-Informations-und-Buerosystemtechnik-Informationselektronikerin-Schwerpunkt-Geraete-Informations-und-Buerosystemtechnik.html' class='timeline-link'>hier</a><span class='link-arrow'>&#129133;</span>",
+  },
+  {
+    year: "2015-2017",
+    title_en: "Work and Travel",
+    title_de: "Auslandsaufenthalt",
+    text_en:
+      "Southeast Asia and New Zealand - " +
+      "Engaged in diverse job opportunities for several months on " +
+      "farms and other work settings, contributing to the local " +
+      "communities while gaining valuable life experiences. " +
+      "During this enriching journey, I had the chance to immerse " +
+      "myself in different cultures, enhancing my English " +
+      "language skills through meaningful interactions with " +
+      "people from various backgrounds. The experience of living " +
+      "and working in foreign countries allowed me to develop a " +
+      "deeper appreciation for global diversity and cultivate " +
+      "intercultural connections that have enriched my worldview.",
+    text_de:
+      "Südostasien und Neuseeland - " +
+      "Während meines Aufenthalts in Südostasien und Neuseeland " +
+      "habe ich über mehrere Monate hinweg vielfältige " +
+      "Tätigkeiten auf Farmen und in anderen Arbeitsumgebungen " +
+      "ausgeübt, wodurch ich aktiv zur lokalen Gemeinschaft " +
+      "beitrug und wertvolle Lebenserfahrungen sammelte. Während " +
+      "dieser bereichernden Reise hatte ich die Chance, in " +
+      "verschiedene Kulturen einzutauchen und meine " +
+      "Englischkenntnisse durch bedeutsame Interaktionen mit " +
+      "Menschen aus unterschiedlichen Hintergründen zu " +
+      "verbessern. Das Leben und Arbeiten in fremden Ländern " +
+      "erlaubte mir, eine tiefere Wertschätzung für die globale " +
+      "Vielfalt zu entwickeln und interkulturelle Verbindungen zu " +
+      "knüpfen, die mein Weltbild bereichert haben.",
+  },
+  {
+    year: "2017-2018",
+    title_en: "Technical Diploma",
+    title_de: "Fachhochschulreife",
+    text_en:
+      "Completed technical studies at vocational school <a href='https://www.t1.bbslu.de/schulformen/berufsoberschule-bos-1/' class='timeline-link'>BOS1-LU</a><span class='link-arrow'>&#129133; </span> in Ludwigshafen, Rhineland-Palatinate, Germany.",
+    text_de:
+      "Besuch der Berufsoberschule <a href='https://www.t1.bbslu.de/schulformen/berufsoberschule-bos-1/' class='timeline-link'>BOS1-LU</a><span class='link-arrow'>&#129133; </span> in Ludwigshafen, Rheinland-Pfalz, Deutschland",
+  },
+  {
+    year: "2018-2019",
+    title_en:
+      "IT-Support-Technician | Bechtle AG contractor at SAP Deutschland SE & Co.KG",
+    title_de:
+      "IT-Support-Techniker | Bechtle AG bei SAP Deutschland SE & Co.KG",
+    text_en:
+      "<ul>" +
+      "<li>Troubleshooted and resolved issues with laptops and " +
+      "mobile via remote support and on-site. Created help desk tickets." +
+      "</li>" +
+      "<li>Trained and supported end-users with software, hardware " +
+      "and network standards and use processes." +
+      "</li>" +
+      "<li>Assisted customers with product selection based on " +
+      "stated needs, proposed use, and budget." +
+      "</li>" +
+      "<li>Installed and configured software and hardware for " +
+      "optimal performance." +
+      "</li>" +
+      "</ul>",
+    text_de:
+      "<ul>" +
+      "<li>Fehlersuche an Laptops und mobilen Geräten über " +
+      "Fernwartung und vor Ort. Erstellen von Helpdesk-Tickets." +
+      "</li>" +
+      "<li>Unterstützung von Endbenutzern bei der Einhaltung von " +
+      "Software-, Hardware- und Netzwerkstandards." +
+      "</li>" +
+      "<li>Hilfe bei der Produktauswahl von Software und Hardware " +
+      "auf Basis der Anforderungen, des vorgesehenen Einsatzes und des Budgets." +
+      "</li>" +
+      "<li>Installation und Konfiguration von Software und Hardware " +
+      "für optimale Leistung." +
+      "</li>" +
+      "</ul>",
+  },
+  {
+    year: "2019-2020",
+    title_en: "IT-Administrator | Quinn Emanuel Urquhart & Sullivan, LLP",
+    title_de: "IT-Administrator | Quinn Emanuel Urquhart & Sullivan, LLP",
+    text_en:
+      "<ul>" +
+      "<li>Granting permissions and access rights through Exchange Online " +
+      "and Active Directory.</li>" +
+      "<li>Maintaining server hardware and software components.</li>" +
+      "<li>Implementing new hardware and software solutions on both " +
+      "server and client sides.</li>" +
+      "<li>Creating step-by-step guides and documentation.</li>" +
+      "<li>Organizing training sessions for new employees.</li>" +
+      "<li>Defined and documented technical support best practices.</li>" +
+      "<li>Deploying updates and patches for Windows workstations.</li>" +
+      "<li>Configuring SCCP and SIP Cisco phones using " +
+      "Cisco Unified Communications Manager (CUCM).</li>" +
+      "</ul>",
+    text_de:
+      "<ul>" +
+      "<li>Gewährung von Berechtigungen und Zugriffsrechten über " +
+      "Exchange Online und Active Directory.</li>" +
+      "<li>Wartung von Server-Hardware- und Softwarekomponenten.</li>" +
+      "<li>Implementierung neuer Hardware- und Softwarelösungen " +
+      "auf Server- und Client-Seite.</li>" +
+      "<li>Erstellen von Schritt-für-Schritt-Anleitungen.</li>" +
+      "<li>Organisation von Schulungen für neue Mitarbeiter.</li>" +
+      "<li>Definieren und dokumentieren von Best Practices " +
+      "für den technischen Support.</li>" +
+      "<li>Bereitstellung von Updates und Patches " +
+      "für Windows Arbeitsstationen.</li>" +
+      "<li>Konfiguration von SCCP und SIP Cisco-Telefonen " +
+      "mit Cisco Unified Communications Manager (CUCM).</li>" +
+      "</ul>",
+  },
+  {
+    year: "2020-2022",
+    title_en: "Senior IT-Specialist | HCL Technologies Germany GmbH",
+    title_de: "Senior IT-Spezialist | HCL Technologies Germany GmbH",
+    text_en:
+      "<ul>" +
+      "<li>Analyzed Hardware and Software issues to identify " +
+      "troubleshooting methods needed for quick remediation.</li>" +
+      "<li>Documented all transactions and support interactions " +
+      "in Ticketing system for future reference and addition to knowledge base.</li>" +
+      "<li>Explained technical information in clear terms " +
+      "to non-technical individuals to promote better understanding.</li>" +
+      "<li>Assembly and commissioning of end-user workstations and " +
+      "setup of laboratory workstations and small servers.</li>" +
+      "<li>Update Management via Windows Server Update Services WSUS.</li>" +
+      "</ul>",
+    text_de:
+      "<ul>" +
+      "<li>Beheben von Hardware- und Softwareproblemen an Desktops, " +
+      "Laptops, Druckern und mobilen Geräten über Fernwartung und vor Ort.</li>" +
+      "<li>Dokumentation aller Schritte im Ticketing-System " +
+      "für zukünftige Referenz und Ergänzung der Datenbank.</li>" +
+      "<li>Endbenutzern den fachgerechten Umgang mit Software und Hardware erklären.</li>" +
+      "<li>Montage und Inbetriebnahme von Endbenutzer-Arbeitsstationen und " +
+      "Einrichtung von Laborarbeitsstationen und kleinen Servern.</li>" +
+      "<li>Update-Management über Windows Server Update Services WSUS.</li>" +
+      "</ul>",
+  },
+  {
+    year: "2022-2023",
+    title_en: "Building a Tinyhouse",
+    title_de: "Bau eines Tinyhouse",
+    text_en:
+      "As an enthusiastic builder and DIY enthusiast, I undertook " +
+      "the ambitious project of designing and constructing a Tiny " +
+      "House from the ground up. This hands-on experience allowed " +
+      "me to develop strong skills in carpentry, construction, " +
+      "and project management. From planning the layout and " +
+      "designing the space-efficient interior to sourcing " +
+      "materials and executing the build, I took on various " +
+      "responsibilities to complete the Tiny House.",
+    // "Click <span class='timeline-link'onClick={handleOpenModal}>here</span> for pictures.",
+    text_de:
+      "Als begeisterter Heimwerker und DIY-Enthusiast habe ich " +
+      "mich an das ambitionierte Projekt gewagt, ein Tiny House " +
+      "von Grund auf zu entwerfen und zu bauen. Diese praktische " +
+      "Erfahrung ermöglichte es mir, meine Fähigkeiten in den " +
+      "Bereichen Schreinerei, Bauwesen und Projektmanagement zu " +
+      "entwickeln. Von der Planung des Layouts und der Gestaltung " +
+      "der platzsparenden Möbel bis hin zur Beschaffung von " +
+      "Materialien und der Ausführung des Baus habe ich mich " +
+      "verschiedenen Herausforderungen gestellt, um mein Tiny House " +
+      "Project fertigzustellen. ",
+    // "<span class='timeline-link tinyhouse-icon'>Hier</span> geht es zu den Bildern.",
+  },
+  {
+    year: "2023",
+    title_en:
+      "Software Developer Training | Harvard's CS50 Course and Project Realization",
+    title_de:
+      "Software Entwickler Schulung | Harvard's CS50 Kurs <br />und zusätzliche Projekte",
+    text_en:
+      "I completed an intensive Web Developer Training through" +
+      "Harvard's renowned CS50 Course, gaining comprehensive" +
+      "knowledge in computer science and web development. This" +
+      "rigorous program equipped me with a strong foundation in" +
+      "programming languages, algorithms, data structures, and" +
+      "modern web technologies. During the training, I actively" +
+      "engaged in various hands-on projects, honing my practical" +
+      "skills in front-end and back-end development. Working on" +
+      "real-world projects allowed me to apply my learning to" +
+      "create dynamic and user-friendly websites, enhancing my" +
+      "proficiency in HTML, CSS, JavaScript, and other relevant" +
+      "frameworks. You can view my certificate " +
+      "<a href='https://courses.edx.org/certificates/63c81fbea7c349d2b11980685afac755' class='timeline-link'>here</a><span class='link-arrow'>&#129133;</span>. ",
+    text_de:
+      "Mit Harvard's renommiertem Web Development Kurs habe ich " +
+      "umfassende Kenntnisse im Bereich Informatik und " +
+      "Webentwicklung erworben. Dank diesem anspruchsvollen " +
+      "Programm und meiner bisherigen Projecte erlangte ich " +
+      "umfassende Kenntnisse mit Programmiersprachen, " +
+      "Algorithmen, Datenstrukturen und modernen Webtechnologien. " +
+      "Während der Ausbildung habe ich aktiv an verschiedenen " +
+      "praxisnahen Projekten gearbeitet und meine praktischen " +
+      "Fähigkeiten in der Frontend- und Backend-Entwicklung " +
+      "verfeinert. Durch die Arbeit an realen Projekten konnte " +
+      "ich mein Wissen anwenden, um dynamische und " +
+      "benutzerfreundliche Websites zu erstellen. Dadurch habe " +
+      "ich meine Fähigkeiten in HTML, CSS, JavaScript und anderen " +
+      "relevanten Frameworks weiter ausgebaut. " +
+      "<a href='https://courses.edx.org/certificates/63c81fbea7c349d2b11980685afac755' class='timeline-link'>Hier</a><span class='link-arrow'>&#129133;</span> " +
+      "geht es zum Zertifikat",
+  },
+
+  {
+    year: "2023 - now",
+    title_en: "Freelance Developer",
+    title_de: "Freiberuflicher Entwickler",
+    text_en:
+      "As a freelance developer, I've worked on diverse web and mobile projects " +
+      "utilizing modern technologies. My expertise spans front-end development with React and TypeScript, " +
+      "back-end solutions using Django with custom OAuth and REST APIs, and mobile applications. " +
+      "I've implemented IoT solutions using C++ for ESP8266 chips and configured server environments " +
+      "with Docker containers, Nginx Reverse Proxy, and firewall settings. My technical versatility extends to database management, " +
+      "having set up and maintained MySQL servers on Raspberry Pi. Currently focused on mobile application development using Bun, " +
+      "TypeScript, and React, I continuously expand my skills across the full development stack.",
+    text_de:
+      "Als freiberuflicher Entwickler habe ich an verschiedenen Web- und Mobile-Projekten mit modernen Technologien gearbeitet. " +
+      "Meine Expertise umfasst Frontend-Entwicklung mit React und TypeScript, Backend-Lösungen mit Django " +
+      "inklusive eigener OAuth- und REST-API-Implementierungen sowie Mobile-Anwendungen. " +
+      "Ich habe IoT-Lösungen mit C++ für ESP8266-Chips umgesetzt und Serverumgebungen " +
+      "mit Docker-Containern, Nginx Reverse Proxy und Firewall-Einstellungen konfiguriert. " +
+      "Meine technische Vielseitigkeit erstreckt sich auf Datenbankverwaltung, " +
+      "wobei ich alte MySQL-Server auf aktuellen Raspberry Pi eingerichtet und gewartet habe. " +
+      "Derzeit konzentriere ich mich auf die Entwicklung mobiler Anwendungen mit Bun, TypeScript und React " +
+      "und erweitere kontinuierlich meine Fähigkeiten im gesamten Entwicklungsbereich.",
+  },
+];
